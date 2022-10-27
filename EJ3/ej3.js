db87cbeb1679e53b06f3d14747c2d7dbe4a4ff9c
MarcosGutierrezMoya <marcos.gutierrez.moya2010@gmail.com>
2022-10-27 15:30:12 +0200

    exercise 3

--- EJ3/ej3.js
+++ EJ3/ej3.js
@@ -2,9 +2,15 @@ console.log("Ej 3");
 
 array1 = [1,0,2,3,4];  
 array2 = [3,5,6,7,8,13];
+
+let sumaArray = [...array2]
 suma(array1,array2);
+
 function suma(arr1,arr2) {
-    for (let i = 0; i < arr2.length; i++) {
-        console.log(arr1[i]+arr2[i]);
+    for (let i = 0; i < arr1.length; i++) {
+        sumaArray[i] = arr1[i]+arr2[i];
     }
+    return sumaArray
 }
+
+console.log(sumaArray);
